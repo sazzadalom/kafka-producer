0d911f0cfc27ea20fbcf97593c35d55bfbd08965
Sazzad Alom <sazzad.alom@mosambee.in>
2024-10-06 21:02:57 +0530

    first commit

---
+++ src/main/java/com/alom_kafka/alom_kafka_producer/AlomKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.alom_kafka.alom_kafka_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlomKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlomKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alom_kafka/alom_kafka_producer/AlomKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alom_kafka.alom_kafka_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlomKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
